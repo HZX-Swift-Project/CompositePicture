86d1a927574b0b0cac79834b0257c4cb4bd21523
遇见 <1418021044@qq.com>
2022-07-19 14:53:50 +0800

    添加.gitignore

---
+++ CompositePicture/DDPictureListView.swift
@@ -0,0 +1,56 @@
+//
+//  DDPictureListView.swift
+//  CompositePicture
+//
+//  Created by Meet on 2022/7/19.
+//
+
+import UIKit
+
+class DDPictureListView: UICollectionView {
+    // 背景图片数组
+    let pictureList = ["", "", "", "", ""]
+    // 标签数组
+    let tagList = ["", "", "", "" ,""]
+    
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension DDPictureListView: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pictureList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+}
+
+extension DDPictureListView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: AdaptSize(50), height: AdaptSize(90))
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: AdaptSize(5), bottom: 0, right: AdaptSize(5))
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return AdaptSize(5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0.01
+    }
+    
+}
---
+++ CompositePicture/UIImage+DDHelper.swift
@@ -0,0 +1,72 @@
+//
+//  UIImage+DDHelper.swift
+//  CompositePicture
+//
+//  Created by Meet on 2022/7/19.
+//
+
+import UIKit
+
+extension UIImage {
+    /// 编辑图片
+    /// - Parameters:
+    ///   - radians: 旋转的弧度
+    ///   - isMirrorH: 是否水平镜像
+    ///   - isMirrorV: 是否垂直镜像
+    /// - Returns: 编辑后的图片
+    func operate(radians: CGFloat = 0.0, isMirrorH: Bool = false, isMirrorV: Bool = false) -> UIImage {
+        if radians == 0.0, !isMirrorH, !isMirrorV {
+            return self
+        }
+        var targetImage = self
+        if isMirrorH {
+            // 水平镜像
+            targetImage = targetImage.flipH()
+        }
+        if isMirrorV {
+            // 垂直镜像
+            targetImage = targetImage.flipV()
+        }
+        return targetImage.rotate(radians: radians)
+    }
+    
+    /// 旋转图片
+    /// - Parameter radians: 旋转弧度数
+    /// - Returns: 编辑后的图片
+    func rotate(radians: CGFloat = 0.0) -> UIImage {
+        if radians == 0.0 {
+            return self
+        }
+        let imageRect = CGRect(origin: .zero, size: size)
+        // 获取旋转后的图片尺寸大小
+        var rotatedRect = imageRect.applying(CGAffineTransform(rotationAngle: CGFloat(radians))).integral
+        rotatedRect.origin.x = 0
+        rotatedRect.origin.y = 0
+        // 开启画布 这里第三个参数 scale 出入0 系统会自动缩放
+        UIGraphicsBeginImageContextWithOptions(rotatedRect.size, false, 0.0)
+        guard let context = UIGraphicsGetCurrentContext() else { return self }
+        // 把旋转点放在屏幕中心
+        context.translateBy(x: rotatedRect.width / 2, y: rotatedRect.height / 2)
+        context.rotate(by: radians)
+        context.translateBy(x: -size.width / 2, y: -size.height / 2)
+        // 开始绘画
+        draw(at: .zero)
+        // 获取图片
+        let rotatedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return rotatedImage ?? self
+    }
+    
+    ///  水平镜像
+    /// - Returns: 处理过后的图片
+    func flipH() -> UIImage {
+       return self.withHorizontallyFlippedOrientation()
+    }
+    
+    /// 垂直镜像
+    /// - Returns: 处理过后的图片
+    func flipV() -> UIImage {
+        // 先绕中心点旋转180度 然后再水平镜像
+        return self.rotate(radians: CGFloat(Double.pi)).withHorizontallyFlippedOrientation()
+    }
+}
